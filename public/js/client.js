95ab300fd4a4c630324fa5597212dd75fceb3899
gion <oancaionutandrei@gmail.com>
2016-04-07 19:26:05 +0200

    Aggiunto interfacciamento con il database

--- public/js/client.js
+++ public/js/client.js
@@ -3,6 +3,7 @@ $(function() {
   // Initialize varibles
   var $window = $(window);
   var $usernameInput = $('.usernameInput'); // Input for username
+  var $passwordInput = $('.passwordInput'); // Input for password
   var $messages = $('.messages'); // Messages area
   var $inputMessage = $('.inputMessage'); // Input message input box
 
@@ -16,15 +17,14 @@ $(function() {
 
   var socket = io();
 
-
   // Sets the client's username
   $( "#btnLogin" ).click(function setUsername () {
     username = $usernameInput.val().trim();
+    password = $passwordInput.val();
     // If the username is valid
-    if (username) {
-
+    if (username && password) {
       // Tell the server your username
-      socket.emit('add user', username);
+      socket.emit('add user', {username : username, email: undefined, password : password});
     }
   });
 
@@ -32,12 +32,12 @@ $(function() {
     console.log(addedUser)
     if(addedUser == false)
     {
-        console.log("ciao")
-        $loginPage.fadeOut();
-        $userHomePage.fadeIn();
-        $loginPage.off('click');
+      console.log("ciao")
+      $loginPage.fadeOut();
+      $userHomePage.fadeIn();
+      $loginPage.off('click');
     }
 
-});
+  });
 
 });
--- server.js
+++ server.js
@@ -1,59 +1,52 @@
-var fs = require('fs');
-var mysql = require('mysql');
-
+var fs = require("fs");
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 
+var Database = require("./lib/database")
+
 var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 // Configuring mysql
-var connection = mysql.createConnection({
-  host : config.database_host,
-  port : config.database_port,
-  user : config.database_user,
-  password : config.database_password,
-  database : config.database_name
-});
-connection.connect(function(error) {
-  if (error) {
-    console.error("error connecting to mysql");
-    return;
-  }
-  console.log("connected to mysql");
-});
+var database = Database.connect(
+  config.database_host,
+  config.database_port,
+  config.database_user,
+  config.database_password,
+  config.database_name
+);
 
 // Configuring socket.io
 io.set('origins', '*:'+config.port);
 io.sockets.on("connection", function(socket) {
   console.log("client connected");
+  var addedUser = false;
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (data) {
+    var username = data.username, email = data.email, password = data.password;
+    database.login(username, email, password, function(status, userData) {
+      if (status) {
+        // login effettuato con successo
+        socket.emit('login', {
+          addedUser: true,
+          userData: userData
+        });
+        socket.username = username;
+      } else {
+        // login fallito
+        socket.emit('login', {
+          addedUser: false
+        });
+      }
+      console.log("login", username, status);
+    });
+  });
 });
 
 // Configuring express
 app.use("/", express.static(__dirname + '/public'));
 server.listen(config.port, "0.0.0.0", function(){
   console.log("sciuar-royale is running on port", config.port);
-});
-
-io.on('connection', function (socket) {
-  var addedUser = false;
-
-// when the client emits 'add user', this listens and executes
-socket.on('add user', function (username) {
-  socket.username = username;
-  // Sostituire con query che si interfaccia al database
-  if(socket.username == "timur" )
-  {
-  //  usernames[username] = username;
-  //  ++numUsers;
-    addedUser = true;
-    var messaggio = username+" si è collegato";
-    console.log(messaggio);
-  }
-  socket.emit('login', {
-    addedUser: addedUser
-  });
-
-});
 });
